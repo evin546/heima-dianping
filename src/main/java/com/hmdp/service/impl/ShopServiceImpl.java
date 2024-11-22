6fbec367492bfa2e49c1d061df4fbcaf8edbd924
evin546 <keloguo9@gmail.com>
2024-11-22 11:26:36 +0800

    完成商户缓存逻辑，利用缓存空对象解决缓存穿透问题

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -11,6 +11,9 @@ import com.hmdp.utils.RedisConstants;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.concurrent.TimeUnit;
+
 /**
  * <p>
  *  服务实现类
@@ -37,15 +40,36 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
             Shop shop = JSONUtil.toBean(shopJson, Shop.class);
             return Result.ok(shop);
         }
+        //缓存命中空值，直接报错
+        if(shopJson != null){
+            return Result.fail("商户不存在！");
+        }
+
         //缓存未命中：
         //查询数据库
         Shop shop = getById(id);
+        //数据库中没有对应信息
         if(shop == null){
+            //缓存写入空值
+            stringRedisTemplate.opsForValue().set(shopCacheKey, "", RedisConstants.CACHE_NULL_TTL, TimeUnit.MINUTES);
             return Result.fail("商户不存在！");
         }
         //更新缓存
-        stringRedisTemplate.opsForValue().set(shopCacheKey, JSONUtil.toJsonStr(shop));
+        stringRedisTemplate.opsForValue().set(shopCacheKey, JSONUtil.toJsonStr(shop), RedisConstants.CACHE_SHOP_TTL, TimeUnit.MINUTES);
         //返回数据
         return Result.ok(shop);
+    }
+
+    @Override
+    public Result update(Shop shop) {
+        Long id = shop.getId();
+        if(id == null){
+            return Result.fail("店铺id不能为空！");
+        }
+        //操作数据库
+        updateById(shop);
+        //更新缓存
+        stringRedisTemplate.delete(RedisConstants.CACHE_SHOP_KEY + id);
+        return Result.ok();
     }
 }
