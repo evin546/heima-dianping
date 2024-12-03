3b2d9093b83f27afa9995b37aaa6e02abaff6e55
evin546 <keloguo9@gmail.com>
2024-12-03 11:36:36 +0800

    完成博主关注取关功能

--- src/main/java/com/hmdp/controller/FollowController.java
+++ src/main/java/com/hmdp/controller/FollowController.java
@@ -1,9 +1,11 @@
 package com.hmdp.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.hmdp.dto.Result;
+import com.hmdp.service.IFollowService;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import javax.annotation.Resource;
 
 /**
  * <p>
@@ -16,5 +18,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/follow")
 public class FollowController {
+    @Resource
+    private IFollowService followService;
+
+    @PutMapping("/{id}/{isFollow}")
+    public Result followUser(@PathVariable("id") Long targetUserId, @PathVariable("isFollow") Boolean isFollow){
+        return followService.followUser(targetUserId, isFollow);
+    }
+
+    @GetMapping("/or/not/{id}")
+    public Result getIsFollow(@PathVariable("id") Long targetUserId){
+        return followService.getIsFollow(targetUserId);
+    }
+
 
 }
--- src/main/java/com/hmdp/service/impl/FollowServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/FollowServiceImpl.java
@@ -1,9 +1,13 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.BooleanUtil;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Follow;
 import com.hmdp.mapper.FollowMapper;
 import com.hmdp.service.IFollowService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.UserHolder;
 import org.springframework.stereotype.Service;
 
 /**
@@ -16,5 +20,34 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class FollowServiceImpl extends ServiceImpl<FollowMapper, Follow> implements IFollowService {
+
+    @Override
+    public Result followUser(Long targetUserId, Boolean isFollow) {
+        Long currentUserId = UserHolder.getUser().getId();
+        if(BooleanUtil.isTrue(isFollow)){
+            //关注：
+            Follow follow = new Follow();
+            follow.setUserId(currentUserId);
+            follow.setFollowUserId(targetUserId);
+            save(follow);
+        }
+        else {
+            //取关
+            remove(new QueryWrapper<Follow>()
+                    .eq("user_id", currentUserId)
+                    .eq("follow_user_id", targetUserId));
+        }
+        return Result.ok();
+    }
+
+    @Override
+    public Result getIsFollow(Long targetUserId) {
+        Long currentUserId = UserHolder.getUser().getId();
+        Integer count = query().eq("user_id", currentUserId)
+                .eq("follow_user_id", targetUserId)
+                .count();
+
+        return Result.ok(count > 0);
+    }
 
 }
